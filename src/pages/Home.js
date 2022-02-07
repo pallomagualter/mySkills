a2557d588f6cf7e9eb403d47693268553d509665
pallomagualter <pallomagualter@gmail.com>
2022-02-07 12:59:12 -0300

    separando estilo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
-    return (
-        <View style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
-            <Text>React Native</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Palloma</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121015'
+  },
+  title: {
+    color: '#fff'
+  }
+});
