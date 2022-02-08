c54cb8f97413dc811337b0c290aa9b0125600de4
pallomagualter <pallomagualter@gmail.com>
2022-02-08 16:50:47 -0300

    FlatList

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   TextInput,
   Platform,
+  FlatList
 } from 'react-native';
 
 import { Button } from "../components/Button";
@@ -37,11 +38,13 @@ export default function App() {
         My Skills
       </Text>
 
-      {
-        mySkills.map(skill => (
-          <SkillCard skill={skill} />
-        ))
-      }
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <SkillCard skill={item} />
+        )}
+      />
 
     </View>
   );
