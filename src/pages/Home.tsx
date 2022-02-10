6dfe8140778c91f378c1115531fa28b6a2d8ea5e
pallomagualter <pallomagualter@gmail.com>
2022-02-10 13:38:49 -0300

    usando Typescript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,23 @@ import {
 import { Button } from "../components/Button";
 import { SkillCard } from "../components/SkillCard";
 
+interface SkillData {
+  id: string;
+  name: string;
+}
+
 export default function App() {
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [gretting, setGretting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill])
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data])
   }
 
   useEffect(() => {
@@ -57,9 +67,9 @@ export default function App() {
 
       <FlatList
         data={mySkills}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={item} />
+          <SkillCard skill={item.name} />
         )}
       />
 
